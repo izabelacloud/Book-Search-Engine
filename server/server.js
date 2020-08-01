11cfbde9871a43fbbcfbbde429b8cc17d12af1f0
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-08-01 14:08:10 -0700

    changes for heroku

--- server/server.js
+++ server/server.js
@@ -42,13 +42,13 @@ app.get('*', (req, res) => {
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    // console.log(`API server running on port ${PORT}!`);
-    // // log where we can go to test our GQL API
-    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
-    app.listen(process.env.PORT || 3000, function(){
-      console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-    });
+    // app.listen(process.env.PORT || 3000, function(){
+    //   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    // });
 
   });
 });
